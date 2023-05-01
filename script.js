46622b2c73b248c107708b2cfd037af3d53fbb79
rickshawwalis <rickshawwallis@gmail.com>
2023-05-01 19:24:26 -0300

    r

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+(produtos = () =>{
   const links = document.querySelectorAll('a[data-product]');
-for (let i = 0; i < links.length; i++) {
-  const link = links[i];
-  link.addEventListener('click', function(event) {
-    event.preventDefault();
-    const product = this.getAttribute('data-product');
-    const message = `Olá, gostaria de comprar o produto ${product}.`;
-    const encodedMessage = encodeURIComponent(message);
-    const url = `https://wa.me/5587988440740?text=${encodedMessage}`;
-    window.open(url, '_blank');
-  });
-}
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const product = this.getAttribute('data-product');
+      const message = `Olá, gostaria de comprar o produto ${product}.`;
+      const encodedMessage = encodeURIComponent(message);
+      const url = `https://wa.me/5587988440740?text=${encodedMessage}`;
+      window.open(url, '_blank');
+    });
+  }
+})()
