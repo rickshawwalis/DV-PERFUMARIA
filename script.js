1b93cc83a1f9bd6ee349c0714a24ca8d9f434fee
rickshawwalis <rickshawwallis@gmail.com>
2023-05-02 11:17:13 -0300

    a

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-(produtos = () =>{
+(produtos = () => {
   const links = document.querySelectorAll('a[data-product]');
   for (let i = 0; i < links.length; i++) {
     const link = links[i];
-    link.addEventListener('click', function(event) {
+    link.addEventListener('click', function (event) {
       event.preventDefault();
       const product = this.getAttribute('data-product');
       const message = `Olá, gostaria de comprar o perfume ${product}.`;
       const encodedMessage = encodeURIComponent(message);
       const url = `https://wa.me/5587988440740?text=${encodedMessage}`;
       window.open(url, '_blank');
-    });
+    })
   }
+})()
+
+(imagem = () =>{
+  const divs = document.querySelectorAll(".a1");
+
+  divs.forEach((div) => {
+    const img = div.querySelector(".img1");
+  
+    div.addEventListener("click", (e) => {
+      const x = e.clientX - e.target.offsetCenter;
+      const y = e.clientY - e.target.offsetCenter;
+  
+      img.style.transformOrigin = `${x}px ${y}px`;
+      img.style.transform = "scale(2)";
+    })
+  
+    div.addEventListener("touchend", () => {
+      img.style.transformOrigin = "center center";
+      img.style.transform = "scale(1)";
+    })
+  })
+  
 })()
